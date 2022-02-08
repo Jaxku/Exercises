4c355433812fe5eafa9a4ebcda003247e48b6b9a
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-02-09 10:00:49 +1300

    squeeky bout to pray
    OOHHH WEEE

--- Print my name.py
+++ Print my name.py
@@ -1,1 +1,7 @@
-print("Yo whats up world it's ya girl Squeeky amen and amen again!!!")
+print("Squeeky: Yo whats up world it's ya girl Squeeky amen and amen again!!!")
+print("Dee: yo whats up world today we at wendys trying out the new chicken fires burger")
+print("Squeeky: OHHH WEEE!")
+print("Dee: Let's get our grace on yo.")
+print("Squeeky: Amen and amen again")
+print("Dee: Lets dig in")
+print("Squeeky: OOOOHHH WEEEE")
