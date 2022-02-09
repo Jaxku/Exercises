41c53caddd5cdcdf65d22f5dc85bf9352387bd59
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-02-10 12:40:41 +1300

    If you are not old enough to get a drivers license

--- Are you old enough to get a drivers license.py
+++ Are you old enough to get a drivers license.py
@@ -1,4 +1,4 @@
-#Moral Age Verification System to see if you are old enough to get a drivers licence
+#Moral Age Verification System to see if you are old enough to get a drivers lic1ence
 
 #The age of the child is a constant
 license_age = 16
@@ -7,3 +7,7 @@ age = int(input("What is your age? :"))
 #Checking system to see the teenager is old enough to get a drivers licence
 if age > license_age:
     print("You are eligible to apply for a Driver's Licence in Aotearoa")
+
+#If you are not old enough to get a drivers license
+if age < license_age:
+    print("You are NOT eligible to apply for a Driver's Licence in Aotearoa")
