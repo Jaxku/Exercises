744ff5137f0c677d9f79ce52f45777e6c0dc0791
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-02-16 09:57:51 +1300

    Finished discounts on discounts if you cant

--- Adult Price or Teenage Price.py
+++ Adult Price or Teenage Price.py
@@ -1,3 +1,5 @@
 age = float(input("Please enter you age to see if you quaifly for out teenager deals: "))
-if age >18
-    print("You are unable to get")
+if age > 18:
+    print("You are unable to get discount, please pay the full price.")
+else:
+    print("You are able to get the discount!")
