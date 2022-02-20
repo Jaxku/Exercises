494b5a7306e10e89c16e897710d03f838f2bb654
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-02-21 12:34:05 +1300

    repeat for hard mode and easy mode if entered wrong

--- Number guessing game.py
+++ Number guessing game.py
@@ -1,9 +1,18 @@
 import random
 number = random.randint(1, 50)
 count = 1
-print(number)
+mode = ""
+while mode != "H" and mode != "E":
+    mode = input("Enter 'H' for Hard mode or 'E' for Easy: ").upper()
+    if mode == "H":
+        num_guesses = 4
+    elif mode == "E":
+        num_guesses = 10
+    else:
+        print("That was not a correct option try again. ")
+
 guess = int(input("Guess the number (1 to 50): "))
-while guess != number and count < 10:
+while guess != number and count < num_guesses:
     count += 1
     if guess < number:
         print("The number is higher!")
