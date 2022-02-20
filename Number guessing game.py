cdd1ee8cc094a85bfae699d6acc7f266db6bff67
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-02-21 12:10:38 +1300

    added number is lower/higher

--- Number guessing game.py
+++ Number guessing game.py
@@ -3,5 +3,9 @@ number = random.randint(1, 10)
 
 guess = int(input("Guess the number: "))
 while guess != number:
+    if guess < number:
+        print("The number is higher!")
+    if guess > number:
+        print("The number is lower!")
     guess = int(input("Wrong, Try again:"))
-    print("You got it!")
+print("You got it!")
