57d6733e2a5843e16ec697f749092a1cf2c9bc84
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-02-23 10:30:39 +1300

    Create Testing Functions.py

---
+++ Testing Functions.py
@@ -0,0 +1,7 @@
+def say_hello(first_name, familyname):
+    return f"Hello {first_name} {other} from a function!"
+
+# Main testing the hello
+name = input("What is your first name: ")
+other = input("What is your last name: ")
+print(say_hello(name, other))
