97a0f9969d5b990a7a703b55d161617436e97f54
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-02-22 14:51:46 +1300

    Update Listing.py

--- Listing.py
+++ Listing.py
@@ -1,10 +1,29 @@
 #list data
 numbers = [10, 20, 30, 40]
-fruit = ["pineapple", "apple", "banana"]
+fruit = ["pineapple", "apple", "banana", "melon", "mango", "orange", "grape"]
 
 #printing the lists
 for i in numbers:
     print(i)
 
+print(" ")
+print("---------------------------------------------------")
+print(" ")
+
 for i2 in fruit:
     print(i2)
+print(len(numbers))
+print(len(fruit))
+print(" ")
+print("---------------------------------------------------")
+print(" ")
+#Printing only a few text strings in a list.
+print(numbers[1])
+print(fruit[2])
+print(fruit[-1])
+
+print(" ")
+print("---------------------------------------------------")
+print(" ")
+
+print(fruit[2:3])
