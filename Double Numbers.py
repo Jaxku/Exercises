0716e6c0f0f43f3812b7fe234bd18ff983c78aef
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-02-24 09:07:11 +1300

    added more

--- Double Numbers.py
+++ Double Numbers.py
@@ -1,3 +1,5 @@
+#Doubling numbers
+
 def calculate_double(amount):
     double = amount * 2
     return double
@@ -6,4 +8,27 @@ def calculate_double(amount):
 question = int(input("How much? "))
 answer = calculate_double(question)
 print(f"Double {question} is {calculate_double(question)}")
+
+
+#Halfing numbers
+
+def calculate_half(amount2):
+    half = amount2 // 2
+    return half
+
+#Main routine
+amount2 = int(input("How much? "))
+answer = calculate_half(amount2)
+print(f"Half of {amount2} is {calculate_half(amount2)}")
+
+#10 more
+
+def calculate_add10(amount3):
+    tenmore = amount3 + 10
+    return tenmore
+
+amount3 = int(input("How much? "))
+answer = calculate_add10(amount3)
+print(f"Half of {amount3} is {calculate_add10(amount3)}")
+
 
