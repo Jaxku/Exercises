b2202a1342a9bc03d303751bf284c4bc578ba46c
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-02-24 08:54:26 +1300

    Double numbers

---
+++ Double Numbers.py
@@ -0,0 +1,8 @@
+def calculate_double(amount):
+    double = amount * 2
+    return double
+
+# Main routine
+question = int(input("How much? "))
+answer = calculate_double(question)
+print(f"Double {question} is {answer}")
